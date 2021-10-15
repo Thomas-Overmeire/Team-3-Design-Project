fba301eee04012fa9ffa5eed0ce45b47f559e1b5
trev3lyan <thomas.overmeire@gmail.com>
2021-10-15 23:22:20 +0800

    Added ADC_qsys

---
+++ synth/adc_qsys/adc_qsys/adc_qsys_bb.v
@@ -0,0 +1,32 @@
+
+module adc_qsys (
+	clk_clk,
+	clock_bridge_sys_out_clk_clk,
+	modular_adc_0_command_valid,
+	modular_adc_0_command_channel,
+	modular_adc_0_command_startofpacket,
+	modular_adc_0_command_endofpacket,
+	modular_adc_0_command_ready,
+	modular_adc_0_response_valid,
+	modular_adc_0_response_startofpacket,
+	modular_adc_0_response_endofpacket,
+	modular_adc_0_response_empty,
+	modular_adc_0_response_channel,
+	modular_adc_0_response_data,
+	reset_reset_n);	
+
+	input		clk_clk;
+	output		clock_bridge_sys_out_clk_clk;
+	input		modular_adc_0_command_valid;
+	input	[4:0]	modular_adc_0_command_channel;
+	input		modular_adc_0_command_startofpacket;
+	input		modular_adc_0_command_endofpacket;
+	output		modular_adc_0_command_ready;
+	output		modular_adc_0_response_valid;
+	output		modular_adc_0_response_startofpacket;
+	output		modular_adc_0_response_endofpacket;
+	output	[0:0]	modular_adc_0_response_empty;
+	output	[4:0]	modular_adc_0_response_channel;
+	output	[11:0]	modular_adc_0_response_data;
+	input		reset_reset_n;
+endmodule
